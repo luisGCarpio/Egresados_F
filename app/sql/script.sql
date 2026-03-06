e5e8c6be7da6d512171104a5957463d416a73f73
LUIS <119624380+luisGCarpio@users.noreply.github.com>
2026-03-06 07:40:34 -0500

    Update script.sql
    update 6/03/2026

--- app/sql/script.sql
+++ app/sql/script.sql
@@ -1,22 +1,37 @@
 CREATE TABLE program (
   id_program INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   program_name VARCHAR(150) NOT NULL,
-  faculty VARCHAR(150) NOT NULL
+  id_faculty INT NOT NULL
+  creation_date DATE,
+  update_date DATE,
+  CONSTRAINT fk_faculty FOREIGN KEY (id_faculty) REFERENCES faculty(id_faculty)
 );
+create table faculty(
+  id_faculty INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR(100) NOT NULL unique
+  creation_date DATE,
+  update_date DATE,
+)
 
 CREATE TABLE academic_levels (
   id_level INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   description VARCHAR(50) NOT NULL
+  creation_date DATE,
+  update_date DATE,
 );
 
 CREATE TABLE employment_statuses (
   id_status INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   description VARCHAR(50) NOT NULL
+  creation_date DATE,
+  update_date DATE,
 );
 
 CREATE TABLE sectors ( 
   id_sector INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   name VARCHAR(100) NOT NULL
+  creation_date DATE,
+  update_date DATE,
 );
 
 CREATE TABLE contract_types (
@@ -24,6 +39,8 @@ CREATE TABLE contract_types (
   contract_name VARCHAR(50) NOT NULL,
   description VARCHAR(150) NOT NULL,
   duration VARCHAR(19) NOT NULL
+  creation_date DATE,
+  update_date DATE,
 );
 
 CREATE TABLE graduates (
@@ -37,6 +54,8 @@ CREATE TABLE graduates (
   id_level INT NOT NULL,
   id_status INT NOT NULL,
   id_program INT NOT NULL,
+  creation_date DATE,
+  update_date DATE,
   CONSTRAINT fk_level FOREIGN KEY (id_level) REFERENCES academic_levels(id_level),
   CONSTRAINT fk_status FOREIGN KEY (id_status) REFERENCES employment_statuses(id_status),
   CONSTRAINT fk_program FOREIGN KEY (id_program) REFERENCES program(id_program)
@@ -53,19 +72,10 @@ CREATE TABLE jobs (
   end_date DATE,
   id_type INT,
   related_to_career BOOLEAN,
+  creation_date DATE,
+  update_date DATE,
   CONSTRAINT fk_graduate FOREIGN KEY (id_graduate) REFERENCES graduates(id_graduate),
   CONSTRAINT fk_sector FOREIGN KEY (id_sector) REFERENCES sectors(id_sector),
-  CONSTRAINT fk_type FOREIGN KEY (id_type) REFERENCES contract_types(id_type)
-);
-
-CREATE TABLE job_interview(
-  id_interview INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  id_graduate INT NOT NULL,
-  currently_employed BOOLEAN,
-  related BOOLEAN,
-  salary DECIMAL (10,2),
-  id_type int,
-  CONSTRAINT fk_graduate FOREIGN KEY (id_graduate) REFERENCES graduates(id_graduate),
   CONSTRAINT fk_type FOREIGN KEY (id_type) REFERENCES contract_types(id_type)
 );
 
@@ -76,16 +86,21 @@ CREATE TABLE continuing_education (
   education_type VARCHAR(100) NOT NULL,
   time VARCHAR(50),
   education_date DATE,
+  creation_date DATE,
+  update_date DATE,
   CONSTRAINT fk_ce_graduate FOREIGN KEY (id_graduate) REFERENCES graduates(id_graduate)
 );
 
 CREATE TABLE work_supervisor (
   id_supervisor INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   id_program INT NOT NULL,
-  nombre_profesor VARCHAR(30) NOT NULL,
+  frist_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
   id_graduate INT NOT NULL,
-  first_name VARCHAR(150) NOT NULL,
   id_job INT NOT NULL,
+  contact_job VARCHAR(150) NOT NULL,
+  creation_date DATE,
+  update_date DATE,
   CONSTRAINT fk_ws_program FOREIGN KEY (id_program) REFERENCES program(id_program),
   CONSTRAINT fk_ws_graduate FOREIGN KEY (id_graduate) REFERENCES graduates(id_graduate),
   CONSTRAINT fk_ws_job FOREIGN KEY (id_job) REFERENCES jobs(id_job)
@@ -100,5 +115,9 @@ CREATE TABLE job_offer (
   area VARCHAR(150) NOT NULL,
   email_contact VARCHAR(150) NOT NULL,
   offer_date DATE NOT NULL,
+  program VARCHAR(150) NOT NULL,
+  creation_date DATE,
+  update_date DATE,
   CONSTRAINT fk_offer_type FOREIGN KEY (id_type) REFERENCES contract_types(id_type)
+  constraint fk_offer_program FOREIGN KEY (program) REFERENCES program(program_name)
 );
